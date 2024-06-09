ac721463d44369f1e7d5b0eaea1abd7759ead84a
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-06-09 15:29:19 +0430

    display the user feedback

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns= [
     path('', views.home, name='home'),
+    path('displayFeedback/', views.displayFeedback, name='displayFeedback'),
     # path('about/', views.about, name='about'),
     # path('contact/', views.contact, name='contact'),
     # path('products/', views.products, name='products'),
