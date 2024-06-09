ea55ff59c7f5a79b42f29374007ac2b820b62e1b
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-06-09 16:15:29 +0430

    feedback model created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+	name = models.CharField(max_length=50)
+	email =  models.CharField(max_length=100)
+	feedback = models.TextField()
+
+	def __str__(self):
+		return(f"{self.name} {self.email}")
