d2ba2624595c0ff1254e187dd6dc7c7872273d12
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-06-09 14:44:32 +0430

    first commit of repo

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns= [
+    path('', views.home, name='home'),
+    # path('about/', views.about, name='about'),
+    # path('contact/', views.contact, name='contact'),
+    # path('products/', views.products, name='products'),
+    # path('product/<int:product_id>/', views.product, name='product'),
+    # path('cart/', views.cart, name='cart'),
+]
