296ca3e7a693ff824ddf50a518152025d4fb9b3e
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-06-10 13:58:17 +0430

    product models added and migrated

--- products/models.py
+++ products/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Feedback(models.Model):
-	name = models.CharField(max_length=50)
-	email =  models.CharField(max_length=100)
-	feedback = models.TextField()
+	name = models.CharField(max_length=100, null=True, blank=True)
+	email =  models.CharField(max_length=100, null=True, blank=True)
+	feedback = models.TextField(null=True, blank=True)
 
 	def __str__(self):
 		return(f"{self.name} {self.email}")
+	
+class Product(models.Model):
+	company = models.CharField(max_length=200, null=True, blank=True)
+	cost = models.CharField(max_length=100, null=True, blank=True)
+	image = models.ImageField(upload_to="products/images", null=True, blank=True)
