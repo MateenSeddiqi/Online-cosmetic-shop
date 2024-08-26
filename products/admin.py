f89742da4c7dba98013a58a3d67c394a7017f709
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-08-26 14:44:38 +0430

    getting user input

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Feedback, Product
+from .models import Feedback, Product, contactUs
 # Register your models here.
 
 admin.site.register(Feedback)
 admin.site.register(Product)
+admin.site.register(contactUs)
